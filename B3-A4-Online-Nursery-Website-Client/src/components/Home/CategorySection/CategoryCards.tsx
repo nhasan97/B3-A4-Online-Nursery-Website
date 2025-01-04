11032015b45a28e3fba76c5ad0416d3bb226ee2f
nhasan97 <nh6069325@gmail.com>
2025-01-05 01:06:57 +0600

    added fallback component for the avatar of CategoryCards component

--- B3-A4-Online-Nursery-Website-Client/src/components/Home/CategorySection/CategoryCards.tsx
+++ B3-A4-Online-Nursery-Website-Client/src/components/Home/CategorySection/CategoryCards.tsx
@@ -6,13 +6,23 @@ const CategoryCards = ({ category }: TCategoryProp) => {
   return (
     <Link to={`/products-page/${category?.category}`}>
       <div>
-        <div className="w-full h-full cursor-pointer p-2 rounded-full border-2 border-[#5D7E5F]">
-          <Avatar className="w-full h-full">
-            <AvatarImage src={category?.image} alt="@shadcn" />
+        <div className="w-full h-full">
+          <Avatar className="w-full h-full cursor-pointer p-2 border-2 border-[#5D7E5F]">
+            <AvatarImage
+              src={category?.image}
+              alt="@shadcn"
+              className=" rounded-full"
+            />
             <AvatarFallback className="bg-[#98b2992f] text-4xl text-[#808080]">
-              {category?.category}
+              <div className="bg-[#98b299] w-[214px] h-[214px] rounded-full animate-pulse "></div>
             </AvatarFallback>
           </Avatar>
+
+          {/*<img
+            src={category?.image}
+            alt={category?.category}
+            className="w-[214px] h-[214px]"
+          />*/}
         </div>
         <p className="text-center text-xl text-[#757575] mt-2">
           {category?.category}
