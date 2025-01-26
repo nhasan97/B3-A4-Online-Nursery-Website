5fe50e5a04de9242db674aa50b8653a7f704ae26
nhasan97 <nh6069325@gmail.com>
2025-01-27 00:15:05 +0600

    made the customer review silder responsive in CustomerReviewSection.tsx

--- B3-A4-Online-Nursery-Website-Client/src/components/modules/SiteComponents/Home/CustomerReviewSection/CustomerReviewSection.tsx
+++ B3-A4-Online-Nursery-Website-Client/src/components/modules/SiteComponents/Home/CustomerReviewSection/CustomerReviewSection.tsx
@@ -30,45 +30,6 @@ const CustomerReviewSection = () => {
   const { isLoading: loadingReviews, data: loadedReviews } =
     reviewApi.useGetOverallReviewsQuery(undefined);
 
-  // var settings = {
-  //   dots: true,
-  //   infinite: true,
-  //   // speed: 500,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4,
-  //   initialSlide: 0,
-  //   autoplay: true,
-  //   speed: 3000,
-  //   autoplaySpeed: 3000,
-  //   cssEase: "linear",
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //         dots: true,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 600,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 2,
-  //         initialSlide: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1,
-  //       },
-  //     },
-  //   ],
-  // };
-
   return (
     <div className="py-10 my-10 md:my-20">
       <Container>
@@ -79,33 +40,62 @@ const CustomerReviewSection = () => {
             <Loading />
           ) : loadedReviews?.data?.length > 0 ? (
             <div className="">
-              {/* <Slider {...settings}> */}
-              <Swiper
-                effect={"coverflow"}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={3}
-                loop={true}
-                coverflowEffect={{
-                  rotate: 0,
-                  stretch: 0,
-                  depth: 100,
-                  modifier: 2.5,
-                  slideShadows: false,
-                }}
-                pagination={{
-                  clickable: true,
-                }}
-                modules={[EffectCoverflow, Pagination]}
-                className="mySwiper"
-              >
-                {loadedReviews?.data?.map((review: TReview) => (
-                  <SwiperSlide>
-                    <ReviewCard key={review._id} review={review}></ReviewCard>
-                  </SwiperSlide>
-                ))}
-                {/* </Slider> */}
-              </Swiper>
+              <div className="flex md:hidden">
+                <Swiper
+                  effect={"coverflow"}
+                  grabCursor={true}
+                  centeredSlides={true}
+                  slidesPerView={1}
+                  loop={true}
+                  coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                    slideShadows: false,
+                  }}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={[EffectCoverflow, Pagination]}
+                  className="mySwiper"
+                >
+                  {loadedReviews?.data?.map((review: TReview) => (
+                    <SwiperSlide>
+                      <ReviewCard key={review._id} review={review}></ReviewCard>
+                    </SwiperSlide>
+                  ))}
+                  {/* </Slider> */}
+                </Swiper>
+              </div>
+
+              <div className="hidden md:flex">
+                <Swiper
+                  effect={"coverflow"}
+                  grabCursor={true}
+                  centeredSlides={true}
+                  slidesPerView={3}
+                  loop={true}
+                  coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                    slideShadows: false,
+                  }}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={[EffectCoverflow, Pagination]}
+                  className="mySwiper"
+                >
+                  {loadedReviews?.data?.map((review: TReview) => (
+                    <SwiperSlide>
+                      <ReviewCard key={review._id} review={review}></ReviewCard>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
             </div>
           ) : (
             <NoData text="No Reviews Found" />
